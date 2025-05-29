be413247ab1aeda8af55bd02cb5ffd4fcfdd220c
marcel <marcel.naegle@web.de>
2025-05-29 16:51:59 +0000

    Add devcontainer setup
    #3

--- tournament-planner-backend/src/test/java/com/tournament/tournament/backend/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.tournament.tournament.backend;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
