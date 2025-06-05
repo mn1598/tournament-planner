02d12832fd393af185866579a02fda0f88427403
marcel <marcel.naegle@web.de>
2025-06-05 17:10:02 +0000

    Create Interface for tournament services
    #26

--- tournament-planner-backend/src/main/java/com/marcel/tournament/backend/controller/TournamentController.java
+++ tournament-planner-backend/src/main/java/com/marcel/tournament/backend/controller/TournamentController.java
@@ -1,38 +1,42 @@
 package com.marcel.tournament.backend.controller;
 
 import com.marcel.tournament.backend.bo.Tournament;
+import com.marcel.tournament.backend.service.ITournamentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/tournaments")
+@RequiredArgsConstructor
 public class TournamentController {
+
+    private final ITournamentService tournamentService;
 
     @GetMapping("/")
     public List<Tournament> getAllTournaments() {
-        return List.of();
+        return tournamentService.getAllTournaments();
     }
 
     @GetMapping("/{name}")
-    public String getTournament(@PathVariable String name) {
-        return name;
+    public List<Tournament> getTournament(@PathVariable String name) {
+        return tournamentService.getTournamentByName(name);
     }
 
     @PostMapping("/")
     public Tournament postTournament(@RequestBody Tournament tournament) {
-        return tournament;
+        return tournamentService.createTournament(tournament);
     }
 
-    @PutMapping("/{id}")
-    public Tournament putTournament(@RequestBody Tournament tournament,
-                                @PathVariable Integer id) {
-        return tournament;
+    @PutMapping("/")
+    public Tournament putTournament(@RequestBody Tournament tournament) {
+        return tournamentService.updateTournament(tournament);
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteTournament(@PathVariable Integer id) {
-        return String.valueOf(id);
+    public Tournament deleteTournament(@PathVariable Integer id) {
+        return tournamentService.deleteTournamentById(id);
     }
 
 }
