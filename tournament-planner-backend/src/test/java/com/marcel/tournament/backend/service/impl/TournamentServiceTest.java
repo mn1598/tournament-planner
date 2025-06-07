071b8f48b929b4a202bed67b865ccad9010e1516
marcel <marcel.naegle@web.de>
2025-06-07 16:51:46 +0000

    Create test classes for Controller and Service
    #26

---
+++ tournament-planner-backend/src/test/java/com/marcel/tournament/backend/service/impl/TournamentServiceTest.java
@@ -0,0 +1,27 @@
+package com.marcel.tournament.backend.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TournamentServiceTest {
+
+    @Test
+    void createTournament() {
+    }
+
+    @Test
+    void getTournamentByName() {
+    }
+
+    @Test
+    void updateTournament() {
+    }
+
+    @Test
+    void deleteTournamentById() {
+    }
+
+    @Test
+    void getAllTournaments() {
+    }
+}
